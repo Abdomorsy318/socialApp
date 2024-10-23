67b894be790b3a5b3ac2bf99de6723567f83705b
Abdo Morsy <abdomorsy318@gmail.com>
2024-10-23 13:49:56 +0300

    v6

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,12 @@ import { SignupComponent } from './components/signup/signup.component';
 import { LoginComponent } from './components/login/login.component';
 import { authGaurdGuard } from './core/auth-guard.guard';
 import { blankGuardGuard } from './core/blank-guard.guard';
+import { NotfoundComponent } from './components/notfound/notfound.component';
 
 export const routes: Routes = [
     {path:"" , redirectTo:"login" , pathMatch:'full'},
-    {path:"timeLine" , component:TimelineComponent, canActivate:[authGaurdGuard]  , title:'TimeLine'},
     {path:"signup" , component:SignupComponent , canActivate:[blankGuardGuard] , title:'SignUp'},
-    {path:"login" , component:LoginComponent , canActivate:[blankGuardGuard]  , title:'Login'}
+    {path:"login" , component:LoginComponent , canActivate:[blankGuardGuard]  , title:'Login'},
+    {path:"timeLine" , component:TimelineComponent, canActivate:[authGaurdGuard]  , title:'TimeLine'},
+    {path:'**' , component:NotfoundComponent , title:'NotFound'}
 ];
